0087cdb0125726478c3f875a108228206d1bdd36
pyperfly <84433506+pyperfly@users.noreply.github.com>
2024-04-03 10:44:48 +0200

    Update CompText.vue

--- src/components/CompText.vue
+++ src/components/CompText.vue
@@ -11,7 +11,7 @@ export default {
 
     data(){
         return{
-            aboutText : 'Sie möchten vitale Bepflanzungen, ein harmonisches Nebeneinander und pflegeleichte Eleganz in Ihrem Garten?\
+            aboutText : 'Vitale Bepflanzungen, ein harmonisches Nebeneinander und pflegeleichte Eleganz in Ihrem Garten?\
              Durch Schnittmaßnahmen, Bodenverbesserungen und Pflanzungen lassen sich mit Pflanzenkenntnis und technischem Geschick Gärten umgestalten.\
             Ein Gehölz-Rückschnitt kann für mehr grün sorgen und das Gehölz wird im Wachstum gebremst. \
             Besonders Interessant kann auch eine Veränderung in ihrer Bepflanzung sein, so manch nicht ganz bekannter Strauch kann ihren Garten aufwerten und doch erstaunlich pflegeleicht sein.\
